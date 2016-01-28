4be23a2184c86b4074ff98a8d7fda10806a03734
Serg09 <sergeyskumatov@aol.com>
2016-01-28 00:28:40 -0700

    Another mailer test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,12 +63,12 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
-  host = 'rocky-ravine-1305.herokuapp.com'
-  config.action_mailer.default_url_options = { host: host }
+  # config.action_mailer.delivery_method = :smtp
+  # host = 'rocky-ravine-1305.herokuapp.com'
+  # config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
-    :port           => '25',
+    :port           => '587',
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
